d3324881e196cd45ffabf3464506f2015ac7cc1d
Dan <39170265+chillenberger@users.noreply.github.com>
2022-05-02 15:25:59 -0600

    recursion pg 160 ex4

--- recursion.py
+++ recursion.py
@@ -9,5 +9,19 @@ def numsum(low, high):
     return high + numsum(low, high-1)
 
 
+# Data Structures and Algorithms
+# pg 160 ex4
+def print_clean_lst(lst):
+    for i in lst:
+        if isinstance(i, list):
+            print_clean_lst(i)
+        else:
+            print(i)
+
+
+
 if __name__ == "__main__":
     print(numsum(0, 100))
+
+    dirty_lst = [1,2,[5, 4], [[],[[],4]],6,7,8]
+    print_clean_lst(dirty_lst)
