858d26c056c172f092354cc0d2ee2f185f8e2085
Dan <39170265+chillenberger@users.noreply.github.com>
2022-05-09 13:46:07 -0600

    Update graph.py
    dijkstra min path

--- graph.py
+++ graph.py
@@ -13,13 +13,15 @@ import random
 class vertex:
     user_id = 0
 
-    def __init__(self, value):
+    def __init__(self, meta):
         # sequential ids
         self.id = vertex.user_id
         # incriment id tracker
         vertex.user_id += 1
-        self.adjacent = {}
-        self.meta={}
+        # container for connected edges
+        self.adjacent = []
+        # container for any data associated with this vertex
+        self.meta=meta
 
     # add an connection between two vertecies.
     def add_adjacent(self, vertex_to_add=None):
@@ -27,13 +29,27 @@ class vertex:
         if type(vertex_to_add) is not vertex:
             return
         # ensure not already added.
-        if not self.adjacent.get(vertex_to_add.id):
-            self.adjacent[vertex_to_add.id] = vertex_to_add
+        if vertex_to_add not in self.adjacent:
+            self.adjacent.append(vertex_to_add)
 
     # add connection to and from two verecies.
     def add_adjacent_mutual(self, vertex_to_add=None):
         self.add_adjacent(vertex_to_add)
         vertex_to_add.add_adjacent(self)
+
+class weighted_vertex:
+    id = 0
+    def __init__(self, meta):
+        self.id = weighted_vertex.id
+        weighted_vertex.id += 1
+        self.adjacent = {}
+
+    def add_adjacent(self, vertex_to_add=None, weight=0):
+        if type(vertex_to_add) is not weighted_vertex:
+            return
+        if vertex_to_add not in self.adjacent:
+            self.adjacent[vertex_to_add] = weight
+
 
 # depth first search because if follows every verticies contact chain to the end
 # before moving on to the next verticies contacts
@@ -41,45 +57,136 @@ def depth_first_search(vertex, visited=None):
     visited = {} if visited is None else visited
     visited[vertex.id] = True
     print(vertex.id)
-    for key, edge in vertex.adjacent.items():
-        if not visited.get(key):
-            depth_first_search(edge, visited)
+    # if weighted graph adjacent in dicts
+    if type(vertex.adjacent) is dict:
+        for edge, weight in vertex.adjacent.items():
+            if not visited.get(edge.id):
+                depth_first_search(edge, visited)
+    # if not weighted graph adjacent in list
+    elif type(vertex.adjacent) is list:
+        for edge in vertex.adjacent:
+            if not visited.get(edge.id):
+                depth_first_search(edge, visited)
 
 # bredth first search because it will add all adjacent verticies to a queue
 # before moving deeper into the graph.
 def breadth_first_search(vertex, visited=None, queue=None):
     visited = {} if visited is None else visited
-    queue = [] if Queue is None else queue
+    queue = [] if queue is None else queue
     visited[vertex.id] = True
     queue.append(vertex)
     while queue:
         vertex = queue[0]
         del queue[0]
         print(vertex.id)
-        for key, adj in vertex.adjacent:
-            if not visited.get(key):
-                visited[key] = True
-                queue.append(adj)
+        if type(vertex.adjacent) is dict:
+            for adj, weight in vertex.adjacent.items():
+                if not visited.get(adj.id):
+                    visited[adj.id] = True
+                    queue.append(adj)
+        elif type(vertex.adjacent) is list:
+            for adj in vertex.adjacent:
+                if not visited.get(adj.id):
+                    visited[adj.id] = True
+                    queue.append(adj)
 
+# one solution to minimum length problem.
+def dijkstra_alogorithm(start, end):
+    print(f"start: {start.id}, end: {end.id}")
+    if type(start) is not weighted_vertex:
+        return
+    if type(end) is not weighted_vertex:
+        return
+    # make our current vertex the start vertex
+    current = start
+    # track min weight from start vertex to all other vertecies
+    cheapest = {}
+    # track min previous vertex weight
+    previous_city = {}
+    # track visited verticies
+    visited = {}
+    # track discovered vertiecies we have not visited.
+    unvisited = {}
+    # set weight to current at zero
+    cheapest[current] = 0
+    while current:
+        # add current vertex to visited
+        visited[current] = True
+        # if current in unvisited, we must remove it
+        if unvisited.get(current):
+            del unvisited[current]
+        # iterate through all adjacent vertices
+        for vertex, weight in current.adjacent.items():
+            # if newly discovered vertex we havent been to, add to unvisited
+            if not unvisited.get(vertex) and not visited.get(vertex):
+                unvisited[vertex] = True
+            # calculate total weigh to get to adjacent vertex from start vertex
+            total_weight = cheapest[current] + weight
+            print(total_weight)
+            # if lower weight than other path in cheapest,
+            if not cheapest.get(vertex) or total_weight < cheapest[vertex]:
+                # replace cheapest value
+                cheapest[vertex] = total_weight
+                # track previous verticie
+                previous_city[vertex] = current
+        # find min weight of unvisited vertices and make that next vertice
+        min = None
+        next = None
+        for key in unvisited.keys():
+            if min is None or cheapest.get(key) < min:
+                min = cheapest[key]
+                next = key
+        current = next
+    path = []
+    current = end
+    print(previous_city)
+    while current is not start:
+        print(f"id: {current.id}, price: {cheapest[current]}")
+        path.append(current.id)
+        current = previous_city[current]
 
+    path.append(start.id)
+    path.reverse()
+    return path, cheapest[end]
 
-
-
-if __name__ == "__main__":
-
+# make graph for testing
+def make_graph(size, weighted=False):
     # build graph nodes
-    max_verticies = 100
+    max_verticies = size
     verticies = {}
     for i in range(max_verticies):
-        verticies[i] = vertex(i)
+        verticies[i] = weighted_vertex(i) if weighted else vertex(i)
 
     # randomly make graph edges.
     for i in random.sample(range(1,max_verticies), max_verticies-1):
         number_of_edges = random.randint(1, max_verticies-1)
         for j in range(number_of_edges):
             adj = random.randint(1, max_verticies-1)
-            verticies[i].add_adjacent(verticies[adj])
+            if weighted:
+                verticies[i].add_adjacent(verticies[adj], random.randint(1, 100))
+            else:
+                verticies[i].add_adjacent(verticies[adj])
+    return verticies
 
-    print(f"vertex: {verticies[4].id}, adj: {verticies[4].adjacent}")
 
-    depth_first_search(verticies[1])
+if __name__ == "__main__":
+
+    # verticies = make_graph(size=10, weighted=True)
+    # print(f"vertex: {verticies[4].id}, adj: {verticies[4].adjacent}")
+    # depth_first_search(verticies[1])
+    # print("*****")
+    # breadth_first_search(verticies[1])
+
+    a = weighted_vertex(1)
+    b = weighted_vertex(2)
+    c = weighted_vertex(3)
+    d = weighted_vertex(4)
+
+    a.add_adjacent(b, 2)
+    b.add_adjacent(c, 3)
+    a.add_adjacent(c, 2)
+    b.add_adjacent(d, 1)
+    c.add_adjacent(d, 2)
+
+    path, price = dijkstra_alogorithm(a, d)
+    print(f"path: {path}, price: {price}")
